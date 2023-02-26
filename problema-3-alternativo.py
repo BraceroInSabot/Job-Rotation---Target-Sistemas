ac6a2ca9f029d65d78b55faf9e95e9ff193aef2b
Guilherme Bracero Gonzales <guibragon@gmail.com>
2023-02-26 15:32:12 -0300

    Arquivo dados.json adicionado e arquivo problema-3.py renomeado

--- problema-3-alternativo.py
+++ problema-3-alternativo.py
@@ -7,7 +7,19 @@ from json import load, loads, dump, dumps
 
 
 """
-Simluação do faturamento mensal de uma empresa representada por 5 semanas
+3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que
+desejar, que calcule e retorne:
+
+• O menor valor de faturamento ocorrido em um dia do mês;
+• O maior valor de faturamento ocorrido em um dia do mês;
+• Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+
+IMPORTANTE:
+
+a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
+
+b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo
+da média;
 """
 
 
@@ -67,7 +79,7 @@ def data_unpacking() -> function:
     """
     data_values = []
 
-    with open("problema-3.json", encoding="utf-8") as file:
+    with open("dados-alternativos.json", encoding="utf-8") as file:
         json_data = load(file)
 
     try:
